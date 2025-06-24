2d1220ed2a1577c7f672fdd49f7cfebfd170ea27
xandersavage <47666196+xandersavage@users.noreply.github.com>
2025-06-24 10:38:43 +0100

    Add Sentry integration, companion session logic, and UI improvements

---
+++ sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://8282fd60a7f2567e2358c35cd8d0765b@o4509543177388037.ingest.us.sentry.io/4509543180664833",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
